d60df73ffa160e37d60cc79c45570b23751cbde0
EvanLyu732 <65656564+EvanLyu732@users.noreply.github.com>
2020-08-04 15:51:50 +0100

    Create FindAllNumbersDisappearedsolution.java

---
+++ Array/FindAllNumbersDisappearedsolution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+          
+        for(int i=0; i<nums.length; i++){
+            int newIndex = Math.abs(nums[i]) - 1;
+            if (nums[newIndex] > 0) {
+                nums[newIndex] *= -1;
+            }
+        }
+        
+        List<Integer> result = new ArrayList<Integer>();
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0){
+                result.add(i+1);
+            }
+        }
+        return result;
+    }
+}
